98025654a481dc5487f63627b22237d9a63285b9
aditanin27 <adityahadiwijaya032@gmail.com>
2025-04-28 14:01:07 +0700

    Delete src directory

--- src/api/index.js
+++
@@ -1,8 +0,0 @@
-// src/api/index.js
-export { authApi } from './auth';
-export { anakApi } from './anak';
-export { tutorApi } from './tutor';
-export { kelompokApi } from './kelompok';
-export {raportApi} from './raport'
-export { prestasiApi } from './prestasi';
-export { fetchWithAuth, handleResponse, createFormData } from './utils';
--- src/utils/constants.js
+++
@@ -1,3 +0,0 @@
-export const API_BASE_URL = 'https://bp.berbagipendidikan.org/api';
-export const STORAGE_TOKEN_KEY = 'berbagipendidikan_token';
-export const IMAGE_BASE_URL = 'https://berbagipendidikan.org/storage/AdminShelter/Shelter/';
